d3715feae6b222042755442f16affd56c363fadc
Charles Beynon <eToThePiIPower@gmail.com>
2015-07-20 23:23:23 -0400

    add indexedArray helper to UI

--- client/app.js
+++ client/app.js
@@ -4,3 +4,13 @@ Template.body.helpers({
     return Polls.find();
   }
 });
+
+// add index to each item
+UI.registerHelper('indexedArray', function(context, options) {
+  if (context) {
+    return context.map(function(item, index) {
+      item._index = index;
+      return item;
+    });
+  }
+});
