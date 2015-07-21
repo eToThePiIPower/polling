414025979582ed3176494055b486928bd2c245eb
Charles Beynon <eToThePiIPower@gmail.com>
2015-07-20 22:04:11 -0400

    add Polls#bootstrap

---
+++ server/bootstrap.js
@@ -0,0 +1,31 @@
+// run this when the meteor server is started
+Meteor.startup(function() {
+
+  // If the polls collection is empty, we bootstrap it with some sample data
+  if (Polls.find().count() === 0) {
+    var samplePolls = [
+      {
+        question: 'What is the best web framework?',
+        choices: [
+          { text: 'Meteor, naturally', votes: 0 },
+          { text: 'Ruby on Rails, of course', votes: 0 },
+          { text: 'Django, for sure', votes: 0 },
+          { text: 'Drupal is a framework too!', votes: 0 },
+        ]
+      },{
+        question: 'Which is the greatest web browser?',
+        choices: [
+          { text: 'Chromium/Chrome', votes: 0 },
+          { text: 'Aurora/Firefox', votes: 0 },
+          { text: 'Links', votes: 0 },
+          { text: 'Something closed source', votes: 0 },
+        ]
+      }
+    ];
+
+    // loop over each sample poll and insert into database
+    _.each(samplePolls, function(poll) {
+      Polls.insert(poll);
+    });
+  }
+});
