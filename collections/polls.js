01b8b1378c65829a943464c99c32aa95ccf8f8f3
Charles Beynon <eToThePiIPower@gmail.com>
2015-07-21 00:58:06 -0400

    publish/subscribe recentPolls

--- collections/polls.js
+++ collections/polls.js
@@ -1,1 +1,12 @@
 Polls = new Mongo.Collection('polls');
+
+if (Meteor.isServer) {
+  // Publish the polls to the client
+  Meteor.publish('recentPolls', function() {
+    return Polls.find({}, {sort: {question: 1}, limit: 3});
+  });
+}
+
+if (Meteor.isClient) {
+  Meteor.subscribe('recentPolls');
+}
