2cb6647347ec4ed9da48ff037dae05f9588f62ce
Charles Beynon <eToThePiIPower@gmail.com>
2015-07-21 01:24:45 -0400

    add simple input checking to addPoll

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ Meteor.methods({
       choices: choices,
       created_at: new Date()
     }
+    // Check types
+    if (!Array.isArray(choices) || typeof question !== "string" ) {
+      throw new Meteor.Error(401, 'Invalid parameter type');
+    }
+    if (choices.length != 3 || question.length < 3 || question.length > 140) {
+      throw new Meteor.Error(401, 'Invalid parameter length');
+    }
+
     Polls.insert(newPoll);
   }
 });
